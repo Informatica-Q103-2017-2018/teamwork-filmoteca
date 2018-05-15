4fedafe92a4d89668afb6e569a3bc0842e1f6165
hriverar <37050791+hriverar@users.noreply.github.com>
2018-05-15 13:57:35 +0200

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/Informatica-Q103-2017-2018/teamwork-filmoteca"""
    This reverts commit 9437d0bff1455633ead90a063e3a61b0ab524904.

--- Filmoteca.c
+++ Filmoteca.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-#define N 5
+#define N 8
 
 typedef struct{
 	char titulo[50];
@@ -8,22 +8,18 @@ typedef struct{
 	float nota;
 }pelicula;
 
-
 //Funci?n ordenar por titulo
 //Funci?n ordenar por nota
 //Funci?n ordenar por g?nero
 void registrapeli(pelicula peli);
 void mostrarpeli(pelicula peli);
 
-
 int main()
 {
 	FILE *pvideoteca;
-	char a; // para el switch
+	char a; // a para el switch
 	int error;
 	pelicula vcatalogo[N];
-	do{
-		printf("\n");
 	do
 	{
 		printf("Que desea hacer: R:Registar, M:Mostrar \n");
@@ -35,49 +31,44 @@ int main()
 				case 'r':	
 					registrapeli(vcatalogo[N]);
 					error=1;
-				break;
+					break;
 					
 				case 'm':
 				case 'M':	
 					mostrarpeli(vcatalogo[N]);
 					error=1;
-				break;
+					break;
 					
 				default:
-					printf("Opcion no disponible \n");
+					printf("Opcion no disponible\n");
 					error=-1;
-				break;
+					break;
 			}
-			fflush(stdin);
 	}while(error<0);
-	}while(error>0);
 	return 0;
 }
 
 
-
 void registrapeli(pelicula peli)
-{FILE *pfilmoteca;
-			pfilmoteca=fopen("Videoteca.txt","a");
-				
-				printf("Titulo de la pelicula:\n");
-				scanf("%[^\n]", peli.titulo);
-				fprintf(pfilmoteca, "-%s\n", peli.titulo);
-				
-				printf("Year en el que se estreno la peli:\n");
-				scanf("%d",&peli.year);
-				fprintf(pfilmoteca, "%d \n", peli.year);
-				
-				printf("Nota de la pelicula:\n");
-				scanf("%f",&peli.nota);
-				fprintf(pfilmoteca, "%.2f \n", peli.nota);
+{
+	FILE *pfilmoteca;
+		pfilmoteca=fopen("Videoteca.txt","a");
+		
+			printf("Titulo de la pelicula:\n");
+			scanf("%[^\n]", peli.titulo);
+			fprintf(pfilmoteca, "%s ", peli.titulo);
 			
-			fclose(pfilmoteca);
-			printf("\n Pelicula registrada correctamente.");
+			printf("Year en el que se estreno la peli:\n");
+			scanf("%d",&peli.year);
+			fprintf(pfilmoteca, "%d ", peli.year);
+			
+			printf("Nota de la pelicula:\n");
+			scanf("%f",&peli.nota);
+			fprintf(pfilmoteca, "%.2f ", peli.nota);
+			
+		fclose(pfilmoteca);
+		printf("\n Pelicula registrada correctamente.");
 }
-
-
-
 
 void mostrarpeli(pelicula peli)
 {
@@ -86,14 +77,13 @@ void mostrarpeli(pelicula peli)
 	
 	pfilmoteca = fopen("videoteca.txt", "r");
 
-	printf("Titulo\t\t\t Year\t\t\t Nota\n");
+	printf("Titulo\t\t Year\t\t Nota\n");
 		for (i = 0; i <=N ; i++) // Leemos el fichero línea a línea de cosas separadas por espacios
 		{  
-		fscanf(pfilmoteca, "%[^\n]", &peli.titulo);
-		fscanf(pfilmoteca, "%i %f",&peli.year, &peli.nota);
-		printf("%s\t\t\t %i\t\t\t %.2f \n",peli.titulo, peli.year, peli.nota);
+		fscanf(pfilmoteca, "%s %i %f", &peli.titulo, &peli.year, &peli.nota);
+		printf("%s\t\t %i\t\t %.2f \n",peli.titulo, peli.year, peli.nota);
 		}
-	fclose(pfilmoteca); 
+	fclose(pfilmoteca);// Cerramos el fichero 
 	}
 
 
