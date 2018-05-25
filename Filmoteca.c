dacf23eb4da529c988743a4d52d0c4e4c6c1dd1f
ddonaire <d.donaire@alumnos.upm.es>
2018-05-25 16:43:16 +0200

    David
    He añadido una función para ordenar por titulo y he arrglado un pequeño fallo de otras. Tenemos un problema para imprimir por pantalla la primera pelicula del fichero al ordenar las pelis

--- Filmoteca.c
+++ Filmoteca.c
@@ -15,7 +15,7 @@ void mostrarpeli( ); //función para mostrar las peliculas que hay registradas
 int devuelveN( ); //función para contar las peliculas que hay registradas
 void ordenayear( ); //función para ordenar las peliculas por año
 void ordenanota( ); //función para ordenar las peliculas por nota
-
+void ordenatitulo( ); //función para ordenar las peliculas por titulo
 
 int main()
 {
@@ -41,22 +41,26 @@ int main()
 			case 3:
 				do
 				{	
-					printf("\n1:por a%co, 2:por nota, 3:regresar\n", 164);
+					printf("\n1: Por titulo, 2: Por a%co, 3: Por nota, 4:regresar \n", 164);
 					scanf("%i",&b);
 				
 					switch(b)
 					{	case 1:
-							ordenayear( );
+							ordenatitulo( );
 						break;
 						
 						case 2:
-							ordenanota( );
+							ordenayear( );
 						break;
 					
 						case 3:
-							printf("Has vuelto\n");
+							ordenanota( );
 						break;
 					
+						case 4:
+						printf("\nHas vuelto \n");
+						break;
+
 						default:
 							printf("\nOpcion no disponible \n");
 						break;
@@ -141,7 +145,7 @@ int devuelveN( )
 
 
 
-void ordenanota( )
+void ordenatitulo( )
 {
 	int N=devuelveN( ); //Para saber el número de peliculas para el vector
 	FILE *pfilmoteca;
@@ -157,21 +161,21 @@ void ordenanota( )
 		i++;
 	}
 	
-	printf("Peliculas ordenadas por a%co: \n",164);
+	printf("Peliculas ordenadas por titulo: \n",164);
 	printf("\nTitulo\t\t\t\t\t A%co\t\tNota\n\n",164);
 	
 	for(i=0; i<=N-1; i++) //Ahora lo ordenamos
 	{
 		for(j=i+1; j<=N-1 ; j++) // Comparamos cada elemento con el siguiente
 		{
-			if (peli[i].nota > peli[j].nota)//Si es mayor intercambiamos el contenido de los dos elementos
+			if (strcmp(peli[i].titulo, peli[j].titulo) > 0)
 			{
 			aux = peli[i];
 			peli[i] = peli[j];
 			peli[j] = aux;
 			}
 		}
-		printf("%-40s %i\t\t%.2f\n",peli[i].titulo, peli[i].year, peli[i].nota);
+		printf("%-40s %i\t\t%.2f",peli[i].titulo, peli[i].year, peli[i].nota);
 	}	
 	fclose(pfilmoteca); 
 }
@@ -193,7 +197,7 @@ void ordenayear( )
 		i++;
 	}
 	
-	printf("Peliculas ordenadas por año: \n");
+	printf("Peliculas ordenadas por a%co: \n",164);
 	printf("\nTitulo\t\t\t\t\t A%co\t\tNota\n\n",164);
 	
 	for(i=0; i<=N-1; i++) //Ahora lo ordenamos
@@ -207,8 +211,46 @@ void ordenayear( )
 				peli[j] = aux;
 			}
 		}
-		printf("%-40s %i\t\t%.2f\n",peli[i].titulo, peli[i].year, peli[i].nota);
+		printf("%-40s %i\t\t%.2f",peli[i].titulo, peli[i].year, peli[i].nota);
 	}
+	fclose(pfilmoteca); 
+}
+
+
+
+
+void ordenanota( )
+{
+	int N=devuelveN( ); //Para saber el número de peliculas para el vector
+	FILE *pfilmoteca;
+	pfilmoteca = fopen("videoteca.txt", "r"); 
+	pelicula peli[N]; //creamos un vector de peliculas
+	pelicula aux; //Necesitamos una variable auxiliar de almacenamiento temporal
+	int i=0, j=0;
+	
+	
+	while (feof(pfilmoteca) == 0) //Primero leemos los datos
+	{
+		fscanf(pfilmoteca, "%[^;]; %i; %f;", peli[i].titulo, &peli[i].year, &peli[i].nota);
+		i++;
+	}
+	
+	printf("Peliculas ordenadas por nota: \n");
+	printf("\nTitulo\t\t\t\t\t A%co\t\tNota\n\n",164);
+	
+	for(i=0; i<=N-1; i++) //Ahora lo ordenamos
+	{
+		for(j=i+1; j<=N-1 ; j++) // Comparamos cada elemento con el siguiente
+		{
+			if (peli[i].nota > peli[j].nota)//Si es mayor intercambiamos el contenido de los dos elementos
+			{
+			aux = peli[i];
+			peli[i] = peli[j];
+			peli[j] = aux;
+			}
+		}
+		printf("%-40s %i\t\t%.2f",peli[i].titulo, peli[i].year, peli[i].nota);
+	}	
 	fclose(pfilmoteca); 
 }
 
